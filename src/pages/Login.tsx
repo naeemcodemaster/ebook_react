08abdf4e2e8b7e51cb778065b119bd9a65ae0168
= <=>
2024-10-26 23:22:55 +0500

    add Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,37 @@
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
 function LoginPage() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <section className="flex justify-center items-center h-screen">
+            <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>Add Email and Password to login with eBook</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Name</Label>
+                            <Input id="email" placeholder="Enter Email" />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="framework">Framework</Label>
+                            <Input id="password" placeholder="Enter Password" />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+
+                <Button>Sign in</Button>
+            </CardFooter>
+        </Card>
+        </section>
+    )
 }
 
 export default LoginPage
