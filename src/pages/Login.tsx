2ce6d185b995885f55e8541d2b6a1c7b94ce5f9f
= <=>
2024-10-27 10:56:50 +0500

    add login functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,52 +2,72 @@ import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { useRef } from "react"
+import { useMutation } from "@tanstack/react-query"
+import { login } from "@/http/api"
+
+
+
 
 function LoginPage() {
+    const navigate = useNavigate();
     const emailRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
+
+
+    // Mutations
+    const mutation = useMutation({
+        mutationFn: login,
+        onSuccess: () => {
+            console.log("Login successfull")
+
+            navigate('/dashboard/home')
+        },
+    })
+
     const handleLogin = () => {
         const email = emailRef.current?.value;
         const password = passwordRef.current?.value;
+        if (!email || !password) {
+            return alert('Please enter email and password');
+        }
+        mutation.mutate({ email, password });
 
-        
-        
 
     }
     return (
         <section className="flex justify-center items-center h-screen">
             <Card className="w-[350px]">
-            <CardHeader>
-                <CardTitle>Login</CardTitle>
-                <CardDescription>Add Email and Password to login with eBook</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="email" ref={emailRef} placeholder="Enter Email" />
+                <CardHeader>
+                    <CardTitle>Login</CardTitle>
+                    <CardDescription>Add Email and Password to login with eBook</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="grid w-full items-center gap-4">
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="email" ref={emailRef} placeholder="Enter Email" />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="framework">Framework</Label>
+                                <Input id="password" ref={passwordRef} placeholder="Enter Password" />
+                            </div>
                         </div>
-                        <div className="flex flex-col space-y-1.5">
-                            <Label htmlFor="framework">Framework</Label>
-                            <Input id="password" ref={passwordRef} placeholder="Enter Password" />
-                        </div>
-                    </div>
-                </form>
-            </CardContent>
-            <CardFooter className="flex justify-between flex-col">
+                    </form>
+                </CardContent>
+                <CardFooter className="flex justify-between flex-col">
 
-                <Button onClick={handleLogin} className="w-full">Sign in</Button>
-                <div className="mt-2 text-center text-sm">
+                    <Button onClick={handleLogin} className="w-full">Sign in</Button>
+                    <div className="mt-2 text-center text-sm">
                         Don't have an account? {' '}
                         <Link to={'/auth/register'}>
                             Sign Up
                         </Link>
                     </div>
-            </CardFooter>
-        </Card>
+                </CardFooter>
+            </Card>
         </section>
     )
 }
