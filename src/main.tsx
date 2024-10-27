96a84b49765021a0a132ac26f061c6ab3efd48b1
= <=>
2024-10-27 10:19:40 +0500

    add tan stack query and axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
 
+
+const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router}>
-
-    </RouterProvider>
-   
+    <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />   
+    </QueryClientProvider>
+ 
   </StrictMode>,
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,8 +3,19 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import { Link } from "react-router-dom"
+import { useRef } from "react"
 
 function LoginPage() {
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const handleLogin = () => {
+        const email = emailRef.current?.value;
+        const password = passwordRef.current?.value;
+
+        
+        
+
+    }
     return (
         <section className="flex justify-center items-center h-screen">
             <Card className="w-[350px]">
@@ -17,18 +28,18 @@ function LoginPage() {
                     <div className="grid w-full items-center gap-4">
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="name">Name</Label>
-                            <Input id="email" placeholder="Enter Email" />
+                            <Input id="email" ref={emailRef} placeholder="Enter Email" />
                         </div>
                         <div className="flex flex-col space-y-1.5">
                             <Label htmlFor="framework">Framework</Label>
-                            <Input id="password" placeholder="Enter Password" />
+                            <Input id="password" ref={passwordRef} placeholder="Enter Password" />
                         </div>
                     </div>
                 </form>
             </CardContent>
             <CardFooter className="flex justify-between flex-col">
 
-                <Button className="w-full">Sign in</Button>
+                <Button onClick={handleLogin} className="w-full">Sign in</Button>
                 <div className="mt-2 text-center text-sm">
                         Don't have an account? {' '}
                         <Link to={'/auth/register'}>
