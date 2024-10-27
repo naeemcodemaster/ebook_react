2e8200ee946ca1bb558663e12318078cb36664e9
= <=>
2024-10-27 10:02:40 +0500

    add dashboard layout

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,30 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter,Navigate } from "react-router-dom";
 import LoginPage from "./pages/Login";
-import Home from "./pages/Home";
+
 import Register from "./pages/Register";
+import DashboardLayout from "./layouts/DashboardLayout";
+import HomePage from "./pages/Home";
+import BooksPage from "./pages/BooksPage";
 const router = createBrowserRouter([
     {
-        path:'/',
-        element:<Home/>
+        path: '/',
+        element: <Navigate to="/dashboard/home" />,
+    },
+    {
+        
+        path:'dashboard',
+        element:<DashboardLayout/>,
+        children:[
+            {
+                path:'home',
+                element:<HomePage/>
+            },
+            {
+                path:'books',
+                element:<BooksPage/>
+            }
+        ],
+        
     },
     {
         path:'/login',
